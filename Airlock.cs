7af4fd6c7bba44a8102181116fa1aa23018e2c0a
Michael Smith <me@murray-mint.co.uk>
2015-04-09 20:48:00 +0100

    Initial commit of basic Airlock code

---
+++ Airlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+
+namespace Airlock
+{
+    class Airlock
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        IMyGridTerminalSystem GridTerminalSystem = null;
+
+        #region CodeEditor
+        void Main()
+        {
+
+        }
+        #endregion
+    }
+}
